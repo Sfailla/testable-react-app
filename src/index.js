d436f37f8e16708cb535f30a02f178843be1b378
Stevo99 <Sfailla1983@gmail.com>
2020-06-12 18:56:21 -0400

    added packages and got rid of boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'normalize.css';
-import './styles/style.scss';
+
+import './index.css';
 import App from './components/app/app';
 
 const app = <App />;
