5661a64de51c71fae737e67cf827bdfa976e5cdf
Stevo99 <Sfailla1983@gmail.com>
2020-06-23 11:22:01 -0400

    added new tests to app component

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
+const renderWithRouter = component => {
+	const history = createMemoryHistory();
+	return {
+		...render(<Router history={history}>{component}</Router>)
+	};
+};
+
+afterEach(cleanup);
+
+// TESTING APP
 describe('App', () => {
 	it('renders App without error', () => {
 		render(<App />);
+	});
+
+	it('should take a snapshot', () => {
+		const { asFragment } = render(<App />);
+		expect(asFragment(<App />)).toMatchSnapshot();
 	});
 });
