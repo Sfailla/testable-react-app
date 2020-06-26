b0ce8d4e74010132703704188cf34e93267bd291
Stevo99 <Sfailla1983@gmail.com>
2020-06-26 15:30:46 -0400

    finished unit testing footer component

--- src/components/footer/Footer.spec.js
+++ src/components/footer/Footer.spec.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import {
-	shallowWithTheme,
-	mountWithTheme
-} from '../../mocks/themeMock';
+import { cleanup } from '@testing-library/react';
+import { mountWithTheme } from '../../mocks/themeMock';
 
 import Footer from './Footer';
 
+let wrapper;
+
+beforeEach(() => {
+	wrapper = mountWithTheme(<Footer />);
+});
+
+afterEach(cleanup);
+
 describe('<Footer /> component unit tests', () => {
 	it('should render 1 <Footer /> component', () => {
-		const wrapper = mountWithTheme(<Footer />);
 		expect(wrapper.find(Footer)).toHaveLength(1);
 	});
 
+	it('should take snapshot that matches component', () => {
+		expect(wrapper.find(Footer)).toMatchSnapshot();
+	});
+
 	it('should render footer component with 1 <footer> and 1 <p> tag', () => {
-		const wrapper = mountWithTheme(<Footer />);
 		expect(wrapper.find('footer')).toHaveLength(1);
 		expect(wrapper.find('p')).toHaveLength(1);
 	});
 
 	it('should have the same text value as component', () => {
-		const wrapper = mountWithTheme(<Footer />);
-		const text = 'created by steven failla &copy; 2020';
-		expect(wrapper.find('p').text()).toEqual(text);
+		const text = `created by steven failla ${'&copy;'} 2020`;
+		// for decoding html entity.  &copy;
+		let elem = document.createElement('textarea');
+		elem.innerHTML = text;
+		let decoded = elem.value;
+		expect(wrapper.find('p').text()).toEqual(decoded);
 	});
 });
