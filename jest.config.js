8f8e82fcc46f900342ce9bb0d5c5d6e0c754bf8e
Stevo99 <Sfailla1983@gmail.com>
2020-06-26 15:30:16 -0400

    added toJson npm package

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	setupFilesAfterEnv: [ '<rootDir>/testSetup.js' ],
+	snapshotSerializers: [ 'enzyme-to-json/serializer' ],
 	moduleFileExtensions: [ 'js', 'json', 'jsx' ],
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
