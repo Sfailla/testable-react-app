b837472826d187f2b5cf1524b61622a555b209bb
Stevo99 <Sfailla1983@gmail.com>
2020-06-27 10:00:12 -0400

    added testing library tests to footer component

--- src/components/footer/Footer.spec.js
+++ src/components/footer/Footer.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { mountWithTheme } from '../../mocks/themeMock';
+import { render } from '../../test-utils';
+import { mountWithTheme } from '../../test-utils/helpers.js';
 import toJson from 'enzyme-to-json';
 
 import Footer from './Footer';
@@ -20,11 +21,6 @@ describe('<Footer /> component unit tests', () => {
 		expect(tree).toMatchSnapshot();
 	});
 
-	it('should render footer component with 1 <footer> and 1 <p> tag', () => {
-		expect(wrapper.find('footer')).toHaveLength(1);
-		expect(wrapper.find('p')).toHaveLength(1);
-	});
-
 	it('should have the same text value as component', () => {
 		const text = `created by steven failla ${'&copy;'} 2020`;
 		// for decoding html entity.  &copy;
@@ -32,5 +28,26 @@ describe('<Footer /> component unit tests', () => {
 		elem.innerHTML = text;
 		let decoded = elem.value;
 		expect(wrapper.find('p').text()).toEqual(decoded);
+	});
+});
+
+describe('<Footer> unit tests', () => {
+	it('renders the footer without error', () => {
+		render(<Footer />);
+	});
+
+	it('should render footer with correct text', () => {
+		const { getByText } = render(<Footer />);
+		const text = 'created by steven failla &copy; 2020';
+		// for decoding html entity.  &copy;
+		let elem = document.createElement('textarea');
+		elem.innerHTML = text;
+		let decoded = elem.value;
+		expect(getByText(decoded)).toBeTruthy();
+	});
+
+	it('should take a snapshot', () => {
+		const { asFragment } = render(<Footer />);
+		expect(asFragment(<Footer />)).toMatchSnapshot();
 	});
 });
