f98f27c0d8829fe3aa4fa12ac241857154a4eb62
Stevo99 <Sfailla1983@gmail.com>
2020-06-27 10:00:33 -0400

    commented out header test file

---
+++ src/components/header/Header.spec.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import {
+// 	mountWithTheme,
+// 	wrapWithRouter,
+// 	shallowWithTheme
+// } from '../../test-utils/helpers';
+
+// import Header from './Header';
+
+// let wrapper;
+// beforeEach(() => {
+// 	wrapper = mountWithTheme(<Header />);
+// });
+
+// describe('<Header /> unit tests', () => {
+// 	it('should render <Header /> 1 time', () => {
+// 		expect(wrapper).toHaveLength(1);
+// 	});
+
+// 	// it('should contain 1 <header> tag', () => {
+// 	// 	expect(wrapper.find('header')).toHaveLength(1);
+// 	// });
+// });
