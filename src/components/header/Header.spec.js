8762dcf444ec86d83786eaa8f02122ab96cf8020
Stevo99 <Sfailla1983@gmail.com>
2020-06-28 16:59:11 -0400

    fixed header test by mocking nav component.  now I can test component without router wrapper.

--- src/components/header/Header.spec.js
+++ src/components/header/Header.spec.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { render, renderWithRouter } from '../../test-utils';
+import { render } from '../../test-utils';
 
 import Header from './Header';
 
+jest.mock('react-router-dom', () => ({ Link: 'Link' }));
+jest.mock('../nav/Navigation', () => () => {
+	return <nav>Navigation</nav>;
+});
+
 describe('<Header /> unit tests', () => {
 	it('should render <Header /> correctly', () => {
-		renderWithRouter(<Header />);
+		render(<Header />);
 	});
 
 	it('should contain h2 tag with correct text value', () => {
-		const { getByText } = render(renderWithRouter(<Header />));
+		const { getByText } = render(<Header />);
 		expect(getByText(/logo/i)).toBeInTheDocument();
 	});
 
 	it('should contain <header> element', () => {
-		const { getByTestId } = render(renderWithRouter(<Header />));
+		const { getByTestId } = render(<Header />);
 		const header = getByTestId('header');
 		expect(header).toBeInTheDocument();
 	});
 
 	it('should take a snapshot of component', () => {
-		const { container } = render(renderWithRouter(<Header />));
+		const { container } = render(<Header />);
 		expect(container).toMatchSnapshot();
 	});
 });
